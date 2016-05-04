beb34465870991abf8d3429a889f964eabf64f4a
Kate Grytsenko <aktinijal@gmail.com>
2016-05-04 16:08:17 +0300

    new popups, some styles

--- js/main.js
+++ js/main.js
@@ -11,6 +11,17 @@ $(document).ready(function(){
         return false;
     });
     
+    $('.form-button').submit(function() {
+        $.ajax({
+        type: "POST",
+        url: "mail.php",
+        data: $(this).serialize()    
+        }).done(function() {
+        alert("Thanks for trust! We`ll contact you.")
+        });
+        return false;
+    });
+    
     /////////HEADER////////  
     $(window).scroll(function() {
     if ($(this).scrollTop() > 300){
@@ -49,8 +60,18 @@ $(document).ready(function(){
     }); 
     
 
-    
-    
-    
+    /////////////MAP
+    var map;
+    function initialize() {
+     var Options = {
+      center: new google.maps.LatLng(56, 37),
+      zoom: 8,
+      mapTypeId: google.maps.MapTypeId.ROADMAP
+     };
+     map = new google.maps.Map(document.getElementById("map_canvas"), Options);
+    }
+    google.maps.event.addDomListener(window, 'load', initialize);
+
+
     
 });
