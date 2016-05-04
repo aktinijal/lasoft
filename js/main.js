b71f1fbd9df8c842b9c7c1e1228435820708635e
Kate Grytsenko <aktinijal@gmail.com>
2016-05-04 17:07:25 +0300

    form center

--- js/main.js
+++ js/main.js
@@ -61,16 +61,16 @@ $(document).ready(function(){
     
 
     /////////////MAP
-    var map;
-    function initialize() {
-     var Options = {
-      center: new google.maps.LatLng(56, 37),
-      zoom: 8,
-      mapTypeId: google.maps.MapTypeId.ROADMAP
-     };
-     map = new google.maps.Map(document.getElementById("map_canvas"), Options);
-    }
-    google.maps.event.addDomListener(window, 'load', initialize);
+//    var map;
+//    function initialize() {
+//     var Options = {
+//      center: new google.maps.LatLng(56, 37),
+//      zoom: 8,
+//      mapTypeId: google.maps.MapTypeId.ROADMAP
+//     };
+//     map = new google.maps.Map(document.getElementById("map_canvas"), Options);
+//    }
+//    google.maps.event.addDomListener(window, 'load', initialize);
 
 
     
